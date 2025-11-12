c46fe63d2c64cc755118eabd33ff71d166910c84
gayas5 <gayasuddin541999@gmail.com>
2025-11-12 20:46:30 +0530

    Remove redundant AWS instance resources

--- output.tf
+++ output.tf
@@ -1,25 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "public_instance" {
-  ami           = "ami-0cae6d6fe6048ca2c"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Public-Instance"
-  }
-}
-
-resource "aws_instance" "private_instance" {   # ✅ Changed name here
-  ami           = "ami-0cae6d6fe6048ca2c"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Private-Instance"
-  }
-}
-
 output "public_ip" {
   value       = aws_instance.public_instance.public_ip
   description = "Public IP Address of EC2 instance"
@@ -29,4 +7,3 @@ output "instance_id" {
   value       = aws_instance.public_instance.id
   description = "Instance ID"
 }
-
