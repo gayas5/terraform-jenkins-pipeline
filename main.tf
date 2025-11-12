c29cefa01935d88d7fa613c8290db2b0d324fa46
gayas5 <gayasuddin541999@gmail.com>
2025-11-12 19:00:41 +0530

    Add subnet_id to public_instance resource
    Added subnet_id to aws_instance resource.

--- main.tf
+++ main.tf
@@ -5,4 +5,13 @@ resource "aws_instance" "public_instance" {
  tags = {
    Name = var.name_tag,
  }
+
+}
+resource "aws_instance" "public_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = "subnet-082209f79cb78956a"  # :point_left: replace with your actual subnet ID
+  tags = {
+    Name = var.name_tag
+  }
 }
