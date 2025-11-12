c6ca77a3b5c8e929c4a2d390b40552e400e8abb1
gayas5 <gayasuddin541999@gmail.com>
2025-11-12 19:24:39 +0530

    Add AWS provider and resources for instances

--- output.tf
+++ output.tf
@@ -1,9 +1,31 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "public_instance" {
+  ami           = "ami-0cae6d6fe6048ca2c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Public-Instance"
+  }
+}
+
+resource "aws_instance" "private_instance" {   # ✅ Changed name here
+  ami           = "ami-0cae6d6fe6048ca2c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Private-Instance"
+  }
+}
+
 output "public_ip" {
- value       = aws_instance.public_instance.public_ip
- description = "Public IP Address of EC2 instance"
+  value       = aws_instance.public_instance.public_ip
+  description = "Public IP Address of EC2 instance"
 }
 
 output "instance_id" {
- value       = aws_instance.public_instance.id
- description = "Instance ID"
+  value       = aws_instance.public_instance.id
+  description = "Instance ID"
 }
